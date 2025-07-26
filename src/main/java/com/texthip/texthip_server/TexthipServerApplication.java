6ede576aa3286c04a5b0f63256b55a070a919a68
Lee-min-gue <immigrationgue@soongsil.ac.kr>
2025-07-26 07:46:46 +0000

    첫 Spring Boot 프로젝트 생성

---
+++ src/main/java/com/texthip/texthip_server/TexthipServerApplication.java
@@ -0,0 +1,13 @@
+package com.texthip.texthip_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexthipServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TexthipServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/texthip/texthip_server/TexthipServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.texthip.texthip_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TexthipServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
