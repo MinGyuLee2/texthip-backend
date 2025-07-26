63659fb83cacfdaefcb8688a4bb4d6c201255569
Lee-min-gue <immigrationgue@soongsil.ac.kr>
2025-07-26 09:58:42 +0000

    feat(user) Add findByemail & findByNickname method, updated user class

--- src/main/java/com/texthip/texthip_server/user/UserRepository.java
+++ src/main/java/com/texthip/texthip_server/user/UserRepository.java
@@ -1,7 +1,10 @@
 package com.texthip.texthip_server.user;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    // 이메일로 사용자를 찾기 위한 메소드
+    Optional<User> findByEmail(String email);
+    // 닉네임으로 사용자를 찾기 위한 메소드
+    Optional<User> findByNickname(String nickname);
 }
