23887a539cb4010d8689b73206df18d32df0e1a6
Lee-min-gue <immigrationgue@soongsil.ac.kr>
2025-08-30 10:44:50 +0000

    fix(auth) 일전 Refactoring에서 미수정된 부분 픽스 @RequestMapping("/api/auth")

--- src/main/java/com/texthip/texthip_server/auth/AuthController.java
+++ src/main/java/com/texthip/texthip_server/auth/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     private final AuthService userService;
