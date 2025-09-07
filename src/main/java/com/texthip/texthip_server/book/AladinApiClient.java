0fbaae9193e5ef63cbaba36de6f14bd4074635c6
Lee-min-gue <immigrationgue@soongsil.ac.kr>
2025-09-07 09:39:43 +0000

    refactor(book): AladinApiClient 검색 기능 및 인코딩 개선
    - 기존 '제목 검색'을 '키워드 검색'으로 변경하여 검색 범위를 확장합니다.
    - 한글 검색어 인코딩 문제를 해결하기 위해 UriComponentsBuilder의 인코딩 방식을 수정합니다.

--- src/main/java/com/texthip/texthip_server/book/AladinApiClient.java
+++ src/main/java/com/texthip/texthip_server/book/AladinApiClient.java
@@ -1,73 +1,82 @@
 package com.texthip.texthip_server.book;
 
 import com.texthip.texthip_server.book.dto.AladinSearchResponseDto;
+import lombok.RequiredArgsConstructor;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-
 @Component
+@RequiredArgsConstructor
 public class AladinApiClient {
 
     private final RestTemplate restTemplate;
 
+    // application-local.yml 파일의 ttb-key 값을 주입받도록 수정
     @Value("${aladin.api.ttb-key}")
     private String ttbKey;
 
+    // application-local.yml 파일의 base-url 값을 주입받도록 수정
     @Value("${aladin.api.base-url}")
     private String baseUrl;
 
-    public AladinApiClient(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
-    // 제목으로 책 검색
-    public AladinSearchResponseDto searchBooksByTitle(String title) {
-        URI uri = UriComponentsBuilder
-                .fromUriString(baseUrl + "/ItemSearch.aspx") 
-                .queryParam("TTBKey", ttbKey)
-                .queryParam("Query", title)
-                .queryParam("QueryType", "Title")
-                .queryParam("MaxResults", 10)
-                .queryParam("start", 1)
-                .queryParam("SearchTarget", "Book")
-                .queryParam("output", "js")
-                .queryParam("Version", "20131101")
-                .build(true)
-                .toUri();
-
-        return restTemplate.getForObject(uri, AladinSearchResponseDto.class);
-    }
-
-    // ISBN으로 책 상세 정보 조회
+     /**
+     * ISBN으로 특정 도서의 상세 정보를 조회합니다.
+     */
     public AladinSearchResponseDto lookupBookByIsbn(String isbn) {
-        URI uri = UriComponentsBuilder.fromUriString(baseUrl + "/ItemLookUp.aspx")
-                .queryParam("TTBKey", ttbKey)
+        URI uri = UriComponentsBuilder
+                .fromUriString(baseUrl + "/ItemLookUp.aspx")
+                .queryParam("ttbkey", ttbKey)
+                .queryParam("itemIdType", "ISBN13")
                 .queryParam("ItemId", isbn)
-                .queryParam("ItemIdType", "ISBN13")
                 .queryParam("output", "js")
                 .queryParam("Version", "20131101")
-                .build(true)
+                .queryParam("OptResult", "fulldescription")
+                .build(true) 
                 .toUri();
-
         return restTemplate.getForObject(uri, AladinSearchResponseDto.class);
     }
 
-    // 베스트셀러 목록 조회
-    public AladinSearchResponseDto getBestsellers(int count) {
-        URI uri = UriComponentsBuilder.fromUriString(baseUrl + "/ItemList.aspx")
-                .queryParam("TTBKey", ttbKey)
+    /**
+     * 키워드로 도서를 검색합니다.
+     */
+    public AladinSearchResponseDto searchBooksByKeyword(String keyword, int start) {
+        URI uri = UriComponentsBuilder
+                .fromUriString(baseUrl + "/ItemSearch.aspx")
+                .queryParam("ttbkey", ttbKey)
+                .queryParam("Query", keyword)
+                .queryParam("QueryType", "Keyword")
+                .queryParam("MaxResults", 20)
+                .queryParam("start", start)
+                .queryParam("SearchTarget", "Book")
+                .queryParam("output", "js")
+                .queryParam("Version", "20131101")
+                .encode(StandardCharsets.UTF_8) 
+                .build()                       
+                .toUri();
+        return restTemplate.getForObject(uri, AladinSearchResponseDto.class);
+    }
+    
+    /**
+     * 베스트셀러 목록을 조회합니다.
+     */
+    public AladinSearchResponseDto getBestsellers(int size) {
+        URI uri = UriComponentsBuilder
+                .fromUriString(baseUrl + "/ItemList.aspx")
+                .queryParam("ttbkey", ttbKey)
                 .queryParam("QueryType", "Bestseller")
-                .queryParam("MaxResults", count) // 상위에서 count만큼만 가져오도록 설정
+                .queryParam("MaxResults", size)
                 .queryParam("start", 1)
                 .queryParam("SearchTarget", "Book")
                 .queryParam("output", "js")
                 .queryParam("Version", "20131101")
-                .build(true)
+                .build(true) 
                 .toUri();
-
         return restTemplate.getForObject(uri, AladinSearchResponseDto.class);
     }
 }
